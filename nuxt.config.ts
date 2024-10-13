7814874ed006661aaae6c95cda77f7cbdbb7a6fa
sharonlhs <121072143+sharonlhs@users.noreply.github.com>
2024-10-13 11:28:21 +0800

    Implement Amplitude

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  // app: {
-  //   baseURL: '/playlist-generator'
-  // },
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://cdn.amplitude.com/libs/analytics-browser-2.11.1-min.js.gz',
+          defer: true
+        },
+        {
+          src: 'https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.8.0-min.js.gz',
+          defer: true
+        },
+        {
+          innerHTML: `
+            window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
+            window.amplitude.init('${process.env.AMPLITUDE_API_KEY}', {"autocapture":{"elementInteractions":true}});
+          `,
+          type: 'text/javascript'
+        }
+      ]
+    }
+  },
   components: [
     {
       path: '~/components',
